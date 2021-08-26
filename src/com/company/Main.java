f3cc2205eacd51afc1c50d2648d49a6e9b175241
Kunal Gupta <kunalgupta@Kunals-MacBook-Pro.local>
2021-08-26 21:33:59 +0530

    hey

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-	    int i = 0;
+	    int s = 2;
+	    int k = 5;
+	    int v = 7;
+	    int z = s +k + v;
+	    int y =  s * k * v;
+	    System.out.println("Sum = " + z);
+	    System.out.println("Multiply = " + y );
     }
 }
