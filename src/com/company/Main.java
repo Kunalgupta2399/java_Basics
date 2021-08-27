540703bf61a2cba904face6c2998d177fad4a12c
Kunal Gupta <kunalgupta@Kunals-MacBook-Pro.local>
2021-08-27 14:25:34 +0530

    hello

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 	    int s = 2;
-	    int k = 5;
+	    int k = 4;
 	    int v = 7;
 	    int z = s +k + v;
 	    int y =  s * k * v;
